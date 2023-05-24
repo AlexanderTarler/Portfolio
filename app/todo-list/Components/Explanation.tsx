0f2066570b4e8084d5d9ff0b731145d803ccbc55
alex <alexander.tarler@gmail.com>
2023-05-24 16:34:03 +0200

    Had to switch out quotes to &quot;

--- app/todo-list/Components/Explanation.tsx
+++ app/todo-list/Components/Explanation.tsx
@@ -4,8 +4,8 @@ export default function Explanation() {
 
     return (<>
         <div id='explanation-text'>
-            In this todo-list, a user can generate a new list item easily by clicking the "New"-button. Currently, I am using the Pokéapi to generate a random pokémon name to add some content.
-            The content can be changed by clicking "Edit", and the whole list-item can be deleted by clicking "Delete".
+            In this todo-list, a user can generate a new list item easily by clicking the &quot;New&quot;-button. Currently, I am using the Pokéapi to generate a random pokémon name to add some content.
+            The content can be changed by clicking &quot;Edit&quot;, and the whole list-item can be deleted by clicking &quot;Delete&quot;.
         </div>
     </>
     );
