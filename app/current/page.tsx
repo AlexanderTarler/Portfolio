97894d38344d4d7f83e8a4260e7bcd46607e5852
alex <alexander.tarler@gmail.com>
2023-06-30 16:27:12 +0200

    Added the current-projects-page and some CSS. Still WIP.

--- app/current/page.tsx
+++ app/current/page.tsx
@@ -9,7 +9,21 @@ export default function Current() {
     return (
         <>
             <main>
-                <h1>Coding</h1>
+                <div id="coding">
+                    <h1 className='project-type'>Coding</h1>
+                    <div className='project-text'>
+                        <h3>Dungeons and Dragons Toolkit</h3>
+                        I am currently working on a web application for Dungeons and Dragons 5E. The goal is to make the experience of running a game of D&D to be as smooth as possible for the DM,
+                        providing needed resources, allowing the DM to save these resources for later and to make the planning phase as smooth as possible.
+                        It is currently not hosted, but will hopefully be up and running soon, and when it is you can find it in my portfolio. </div>
+                </div>
+                <div id="game-development">
+                    <h1 className='project-type'>Game Development</h1>
+                    <div className='project-text'>
+                        <h3>Untitled Horror Game</h3>
+                        I am creating a horror game where the goal is to survive being hunted. The player must find a way to evade the invader while escaping the area.
+                    </div>
+                </div>
             </main>
         </>
     )
